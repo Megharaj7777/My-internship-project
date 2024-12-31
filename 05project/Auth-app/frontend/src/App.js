092bb8599753385a778b348de8b1adb4a22f27f8
Megharaj7777 <megharajugowda77@gmail.com>
2024-12-31 17:09:18 +0530

    added 05project.

---
+++ 05project/Auth-app/frontend/src/App.js
@@ -0,0 +1,27 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+import { useState } from 'react';
+
+
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+
+
+  return (
+    <div className="App">
+      
+      <Routes>
+        <Route path='/' element={<Navigate to="/login" />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/home' element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
